4f05cd7190b513a669ee847388534d3219417fcf
Ruman Saleem <ruman63@gmail.com>
2019-02-09 13:24:38 +0530

    add autoprefixer

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,7 +5,13 @@ require('laravel-mix-tailwind')
 mix.js('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.sass', 'public/css')
   .tailwind()
-  .purgeCss()
+  .options({
+    postCss: [ 
+      require('autoprefixer')({
+        browsers: ['last 40 versions'],
+      }) 
+    ]
+  }).purgeCss()
 
 if (mix.inProduction()) {
   mix.version()
