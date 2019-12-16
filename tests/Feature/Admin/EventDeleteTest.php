add372bb9e8e53ee3a4e2127e6c6941e47251b88
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-12-16 20:21:18 +0530

    add test for delete event

---
+++ tests/Feature/Admin/EventDeleteTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Event;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class EventDeleteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function admin_can_delete_event_if_it_has_not_started()
+    {
+        $event = create(Event::class, 1);
+        $this->withoutExceptionHandling()->signInAdmin();
+
+        $this->delete(route('admin.events.delete', $event));
+        $this->assertDeleted('events', $event->toArray());
+    }
+
+    /** @test */
+    public function admin_can_not_delete_event_if_it_has_started()
+    {
+        $event = create(Event::class, 1, [
+            'started_at' => now(),
+        ]);
+        $this->signInAdmin();
+
+        $response = $this->delete(route('admin.events.delete', $event));
+        $response->assertStatus(403);
+    }
+}
