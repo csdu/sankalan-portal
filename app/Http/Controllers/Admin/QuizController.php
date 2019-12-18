fa48bbe38e789f654b17b05ac47757f00ee959a0
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-12-18 16:48:45 +0530

    add delete and update methods

--- app/Http/Controllers/Admin/QuizController.php
+++ app/Http/Controllers/Admin/QuizController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use App\Models\Quiz;
+use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -65,5 +67,78 @@ class QuizController extends Controller
             'message' => 'All quiz responses have been evaluated',
             'scores' => $scores->toArray(),
         ];
+    }
+
+    public function create()
+    {
+        $events = Event::all();
+
+        return view('admin.quizzes.create')->withEvents($events);
+    }
+
+    public function store(Request $request)
+    {
+        request()->validate([
+            'title' => 'required',
+            'instructions' => 'required',
+            'time_limit' => 'numeric',
+            'questions_limit' => 'numeric',
+            'event_id' => 'exists:events,id',
+        ]);
+
+        Quiz::create([
+            'title' => $request->title,
+            'slug' => str_slug($request->title),
+            'instructions' => $request->instructions,
+            'time_limit' => $request->time_limit,
+            'questions_limit' => $request->questions_limit,
+            'event_id' => $request->event_id,
+        ]);
+
+        flash('Quiz created!')->success();
+
+        return redirect()->route('admin.quizzes.index');
+    }
+
+    public function edit(Quiz $quiz)
+    {
+        $events = Event::all();
+
+        return view('admin.quizzes.edit', compact(['quiz', 'events']));
+    }
+
+    public function update(Request $request, Quiz $quiz)
+    {
+        request()->validate([
+            'title' => 'required',
+            'instructions' => 'required',
+            'time_limit' => 'numeric',
+            'questions_limit' => 'numeric',
+            'event_id' => 'exists:events,id',
+        ]);
+
+        $quiz->update([
+            'title' => $request->title,
+            'slug' => str_slug($request->title),
+            'instructions' => $request->instructions,
+            'time_limit' => $request->time_limit,
+            'questions_limit' => $request->questions_limit,
+            'event_id' => $request->event_id,
+        ]);
+
+        flash('Quiz updated!')->success();
+
+        return redirect()->route('admin.quizzes.index');
+    }
+
+    public function delete(Quiz $quiz)
+    {
+        // can only delete quiz which is not active
+        abort_if($quiz->opened_at, 403, "You can only delete quiz which is'nt active");
+
+        $quiz->delete();
+        flash('Quiz deleted', 'success');
+
+        return redirect()->route('admin.quizzes.index');
     }
 }
