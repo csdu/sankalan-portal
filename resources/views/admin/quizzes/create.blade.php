d6b07e64ad6ed254fae2cc9eca47ec4b5d836dca
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-12-19 14:01:35 +0530

    warp label input within a div with margin bottom

--- resources/views/admin/quizzes/create.blade.php
+++ resources/views/admin/quizzes/create.blade.php
@@ -14,35 +14,45 @@
         <form action="{{ route('admin.quizzes.store') }}" method="POST">
             @csrf
 
-            <label class="control">
-                Title
-            </label>
-            <input class="control" name="title" type="text" placeholder="Sankalan" value="{{ old('title') }}" required>
-
-            <label class="control">
-                Instructions
-            </label>
-            <textarea class="control" name="instructions" type="text" required>{{ old('instructions') }}</textarea>
-
-            <label class="control">
-                Time limit <small>in seconds</small>
-            </label>
-            <input class="control" name="time_limit" type="number" value="1800" required>
+            <div class="mb-2">
+                <label class="control">
+                    Title
+                </label>
+                <input class="control" name="title" type="text" placeholder="Sankalan" value="{{ old('title') }}" required>
+            </div>
+       
+            <div class="mb-2">
+                <label class="control">
+                    Instructions
+                </label>
+                <textarea class="control" name="instructions" type="text" required>{{ old('instructions') }}</textarea>
+            </div>
+       
+            <div class="mb-2">
+                <label class="control">
+                    Time limit <small>in seconds</small>
+                </label>
+                <input class="control" name="time_limit" type="number" value="1800" required>
+            </div>
+       
+            <div class="mb-2">
+                <label class="control">
+                    Question limit
+                </label>
+                <input class="control" name="questions_limit" min="1" type="number" value="{{ old('questions_limit', 1) }}" required>
+            </div>
+       
+            <div class="mb-2">
+                <label class="control">
+                    Event
+                </label>
+                <select class="control" name="event_id" required>
+                    @foreach ($events as $event)
+                        <option value="{{ $event->id }}">{{ $event->title }}</option>
+                    @endforeach
+                </select>
+            </div>
             
-            <label class="control">
-                Question limit
-            </label>
-            <input class="control" name="questions_limit" min="1" type="number" value="{{ old('questions_limit', 1) }}" required>
-
-            <label class="control">
-                Event
-            </label>
-            <select class="control" name="event_id" required>
-                @foreach ($events as $event)
-                    <option value="{{ $event->id }}">{{ $event->title }}</option>
-                @endforeach
-            </select>
-
             <button class="btn is-blue" type="submit">Create</button>
             <a href="{{ route('admin.quizzes.index') }}" class="btn">Cancel</a>
         </form>
