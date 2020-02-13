d184be673bc08e9eb296275e28601bb82ad8582b
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2020-02-13 13:21:03 +0530

    fix: question attachment factory bug

--- database/factories/QuestionAttachmentFactory.php
+++ database/factories/QuestionAttachmentFactory.php
@@ -3,10 +3,11 @@
 use App\Models\Question;
 use App\Models\QuestionAttachment;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(QuestionAttachment::class, function (Faker $faker) {
     return [
         'question_id' => factory(Question::class)->create()->id,
-        'path' => 'illustrations/'.$faker->image(),
+        'path' => 'illustrations/'.Str::random(),
     ];
 });
