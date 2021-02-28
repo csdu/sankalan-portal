838d56a22cbe97021a35bd83c0122d46befbcc27
Ruman Saleem <ruman63@gmail.com>
2021-02-28 15:11:37 +0530

    fix users table seeder

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -13,13 +14,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\Models\User')->create([
+        User::factory()->create([
             'first_name' => 'Default',
             'last_name' => 'User',
             'email' => 'sankalan@ducs.in',
         ]);
 
-        factory('App\Models\User')->create([
+        User::factory()->create([
             'first_name' => 'admin',
             'last_name' => 'admin',
             'email' => 'admin@admin.com',
@@ -27,13 +28,13 @@ class UsersTableSeeder extends Seeder
             'is_admin' => true,
         ]);
 
-        factory('App\Models\User')->create([
+        User::factory()->create([
             'first_name' => 'nonadmin',
             'last_name' => 'nonadmin',
             'password' => bcrypt('password'),
             'email' => 'nonadmin@admin.com',
         ]);
 
-        factory('App\Models\User', 7)->create();
+        User::factory()->count(7)->create();
     }
 }
